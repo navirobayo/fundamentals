40a34f72f66fd893cdb2df07c28bcefa3c8855a8
Ivan <navirobayo@gmail.com>
2023-08-12 14:53:54 -0500

    Study

--- SOLID/single_responsability_principle.dart
+++ SOLID/single_responsability_principle.dart
@@ -1,2 +1,55 @@
 /* A class should do only one thing and therefore
 it should have only one reason to change. */
+
+// Example.
+
+void main() {}
+
+class Villain {
+  String name;
+  String superPower;
+  String weakness;
+  String catchPhrase;
+  String hideout;
+  int numberOfSkills;
+  int ageInYears;
+
+  Villain(this.name, this.superPower, this.weakness, this.catchPhrase,
+      this.hideout, this.numberOfSkills, this.ageInYears);
+}
+
+class VillainAppearances {
+  Villain villain;
+  int numberOfAppearancesInComics;
+  int numberOfAppearancesInMovies;
+  int numberOfAppearancesInSeries;
+
+  VillainAppearances(this.villain, this.numberOfAppearancesInComics,
+      this.numberOfAppearancesInMovies, this.numberOfAppearancesInSeries);
+}
+
+class EvilCalculator {
+  int calculateEvilness(villain villain) {
+    return villain.numberOfSkills * villain.ageInYears;
+  }
+}
+
+class villainOneInformation {
+  String name = "The Martian";
+  String superPower = "Teleportation";
+  String weakness = "Water";
+  String catchPhrase = "Hasta la vista, baby";
+  String hideout = "Mars";
+  int numberOfSkills = 3;
+  int ageInYears = 500;
+}
+
+class villainTwoInformation {
+  String name = "The Joker";
+  String superPower = "Intelligence";
+  String weakness = "Batman";
+  String catchPhrase = "Why so serious?";
+  String hideout = "Gotham";
+  int numberOfSkills = 5;
+  int ageInYears = 40;
+}
